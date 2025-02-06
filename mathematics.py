3fc0c601f69ada7fd34dafcbec66d3e7e6d2c657
margherita001 <marghe.altin@gmail.com>
2025-02-06 11:23:24 +0100

    Add mathematics operations

---
+++ mathematics.py
@@ -0,0 +1,5 @@
+def power(a,b):
+    return a**b
+
+def root(a,n):
+    return a**(1 / n)
---
+++ test_mathematics.py
@@ -0,0 +1,8 @@
+from mathematics import *
+
+def test_power():
+    assert power(2,3) == 8
+
+def test_root():
+    assert root(16,2) == 4
+    assert root(16,4) == 2
